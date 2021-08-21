9b10cf7ee95bec8e1466f48ddda69cb1caead367
legomezr2021 <gomez.luis74@gmail.com>
2021-08-21 02:44:33 -0500

    Implementa Consumo Datos Generales Cliente

---
+++ Controller/ConsumeDatosGeneralesCliente.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Controller
+{
+    class ConsumeDatosGeneralesCliente
+    {
+        public DatosGeneralesClienteResponse GetGeneralDataClient(int pidCliente, string pusr, string ppsw)
+        {
+            //Crea objetos para seguridad
+            ConsumeLogin consumelogin = new ConsumeLogin();
+            Seguridad miseguridad = new Seguridad();
+            //Obtiene Crdenciales
+            miseguridad = consumelogin.GetSecurity(pusr, ppsw);
+            //Crea objeto a serializar
+            DatosGeneralesClienteRequest datosgeneralesclienteequestReq = new DatosGeneralesClienteRequest
+            {
+                idCliente = pidCliente,
+                seguridad = miseguridad
+            };
+            //Serializa peticion del servicio
+            string body = JsonConvert.SerializeObject(datosgeneralesclienteequestReq, Formatting.Indented);
+            //Genera llamado a servicio de seguridad
+            var client = new RestClient("https://krsrdgwsw.xicantel.com:8453/auth/api/financial/data/general");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            // Arma y ejecuta servicio 
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            string json = response.Content.ToString();
+            // Obtener la propiedades en una lista 
+            JObject objectres = JObject.Parse(json);
+            Console.WriteLine("Objeto parseado " + objectres);
+            // Serializa resultados JSON a un objeto .NET
+            DatosGeneralesClienteResponse searchResult = JsonConvert.DeserializeObject<DatosGeneralesClienteResponse>(objectres.ToString());
+            return searchResult;
+        }
+    }
+}
---
+++ Model/DatosGeneralesClienteResponse.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Model
+{
+    class DatosGeneralesClienteResponse
+    {
+        [JsonProperty("nombre")]
+        public string nombre { get; set; }
+        [JsonProperty("aPaterno")]
+        public string aPaterno { get; set; }
+        [JsonProperty("aMaterno")]
+        public string aMaterno { get; set; }
+        [JsonProperty("fechaNacimiento")]
+        public long fechaNacimiento { get; set; }
+        [JsonProperty("curp")]
+        public string curp { get; set; }
+        [JsonProperty("rfc")]
+        public string rfc { get; set; }
+        [JsonProperty("tipoIdentificacion")]
+        public string tipoIdentificacion { get; set; }
+        [JsonProperty("noIdentificacion")]
+        public string noIdentificacion { get; set; }
+        [JsonProperty("nacionalidad")]
+        public string nacionalidad { get; set; }
+        [JsonProperty("paisNacimiento")]
+        public string paisNacimiento { get; set; }
+        [JsonProperty("entidadFederativaNacimiento")]
+        public string entidadFederativaNacimiento { get; set; }
+        [JsonProperty("genero")]
+        public string genero { get; set; }
+        [JsonProperty("edoCivil")]
+        public string edoCivil { get; set; }
+        [JsonProperty("escolaridad")]
+        public string escolaridad { get; set; }
+        [JsonProperty("ocupacion")]
+        public string ocupacion { get; set; }
+        [JsonProperty("scoreBuro")]
+        public string scoreBuro { get; set; }
+        [JsonProperty("code")]
+        public string code { get; set; }
+
+    }
+}
